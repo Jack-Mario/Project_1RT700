13d0a29c9e84065eb1aa174c3b181b4f12003392
davidolsonn <david.olson2001@gmail.com>
2026-02-02 17:12:34 +0100

    bargraph fixad igen typ

--- Entillfil.py
+++ Entillfil.py
@@ -1,5 +1,22 @@
+%matplotlib qt
+
+jebvliBEV
 import pandas as pd
+import matplotlib.pyplot as plt
 
 data = pd.read_csv("training_data_VT2026.csv")
 
-print(data)
+data["increase_stock"] = data["increase_stock"].map({
+    "low_bike_demand": 0,
+    "high_bike_demand": 1
+})
+
+counts = data.groupby("hour_of_day")["increase_stock"].sum()
+
+counts.plot(kind="bar")
+plt.figure()
+
+plt.xlabel("Hour of day")
+plt.ylabel("High bike demand frequency")
+plt.title("High bike demand per hour")
+plt.show()
