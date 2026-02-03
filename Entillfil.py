4fb8ac0d2bed2c8a504adeead6819c30ef53ed42
Jack Mario <jack@example.com>
2026-02-03 10:10:11 +0100

    fixade lite bilder/grafer

--- Entillfil.py
+++ Entillfil.py
@@ -8,7 +8,11 @@ data["increase_stock"] = data["increase_stock"].map({
     "high_bike_demand": 1,
 })
 
-hourly_mean = data.groupby("hour_of_day")["increase_stock"].mean()
-hourly_mean.plot(kind='line')                                 
+hourly_mean = data.groupby("precip")["increase_stock"].mean()
+hourly_mean.plot(kind='bar')                                 
 #data.plot(x='hour_of_day', y='increase_stock', kind='hist')
+plt.ylabel("Mean of increase stock")
+plt.title("Mean of increase stock depending on the precipitation")
 plt.show()
+
+print(data["precip"])
