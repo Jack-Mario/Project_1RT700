aeb269fdc45267ebcea2ec8b9d80bfd3d5493d07
Tim Slettenmark Gille <tim.sgille@gmail.com>
2026-02-18 13:18:32 +0100

    naive

--- naive_model.py
+++ naive_model.py
@@ -5,11 +5,11 @@ def naive_model(x_train, x_test, y_train, y_test, seed=0):
 
     rng = np.random.default_rng(seed)
 
-    # Räkna klassfördelning från träningsdatan
+    # beräkna vikterna
     classes, counts = np.unique(y_train, return_counts=True)
     probs = counts / counts.sum()
 
-    # Slumpa prediction enligt samma fördelning
+    # välj postiv eller negativ slumpmässigt enligt fördelningen
     y_pred = rng.choice(classes, size=len(y_test), p=probs)
 
     performance_metrics = [
